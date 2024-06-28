a75b1e4f026d35e6f06e982b682609403ec957f5
Jaime Rodriguez Dennis <jairoden181203@gmail.com>
2024-06-28 12:59:51 -0700

    db sqlite

---
+++ app/src/main/java/com/example/mykotlinapp/database/Alumno.kt
@@ -0,0 +1,9 @@
+package com.example.mykotlinapp.database
+
+data class Alumno(var id: int = 0,
+    var matricula : String = " ",
+    var nombre : String=" ",
+    var domicilio : String=" ",
+    var especialidad : String=" "
+    var foto : String=" "
+    )
---
+++ app/src/main/java/com/example/mykotlinapp/database/dbAlumnos.kt
@@ -0,0 +1,9 @@
+package com.example.mykotlinapp.database
+
+import android.content.Context
+
+class dbAlumnos (private val context: Context) {
+    private val dbHelper : AlumnosDbHelper = AlumnosDbHelper(Context)
+    private val
+
+}
